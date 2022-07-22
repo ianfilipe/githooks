f003bff1e2b74509eb9e65fb09e57a727dfb4ba0
Ian <ssianfilipe@gmail.com>
2022-07-22 17:57:13 -0300

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [initialRepositories, setInitialRepositories] = useState([]);
+  const [repositories, setRepositories] = useState([]);
+
+  const handleOnChange = (event) => {
+    let filteredData = initialRepositories.filter((repo) =>
+      repo.name.match(event.target.value)
+    );
+    setRepositories(filteredData);
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        "https://api.github.com/users/ianfilipe/repos",
+        {
+          headers: {
+            Authorization: "token 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",
+          },
+        }
+      );
+      const data = await response.json();
+      setInitialRepositories(data);
+      setRepositories(data);
+    }
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const filtered = repositories.filter((repo) => repo.favorite);
+    document.title = `Você tem ${filtered.length} favoritos`;
+  }, [repositories]);
+
+  function handleFavorite(id) {
+    const newRepositories = repositories.map((repo) => {
+      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo;
+    });
+    setRepositories(newRepositories);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input
+        onChange={handleOnChange}
+        className="input"
+        placeholder="Pesquise por um repositório"
+      />
+      <ul>
+        {repositories.map((repo) => (
+          <li key={repo.id}>
+            <a target="_blank" href={repo.svn_url}>
+              {repo.name}
+            </a>
+            {repo.favorite && <span className="favorite">(Favorito)</span>}
+            <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+            <span className="visibility">{repo.visibility}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
-
-export default App;
